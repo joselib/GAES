6469113c0dea391b01dc90f3c68ce9fb9bd4f70b
joselib <joselrn01@gmail.com>
2024-10-01 21:15:45 -0500

    add second

---
+++ config/app.php
@@ -0,0 +1,17 @@
+<?php
+
+	const APP_URL="http://localhost/GAES/";
+	const APP_NAME="GAES";
+	const APP_SESSION_NAME="GAES";
+
+
+	/*----------  Zona horaria  ----------*/
+
+    
+	/*
+		Configuración de zona horaria de tu país, para más información visita
+		http://php.net/manual/es/function.date-default-timezone-set.php
+		http://php.net/manual/es/timezones.php
+	*/
+
+	date_default_timezone_set("America/Bogota");
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once "./config/app.php";
+    require_once "./autoload.php";
+
+    if(isset($_GET['views'])){
+        $url=explode("/",$_GET['views']);
+    }else{
+        $url=["welcome"];
+    }
+    ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php require_once "./app/views/inc/head.php"; ?>
+</head>
+<body>
+    <?php require_once "./app/views/inc/script.php"; ?>
+</body>
+</html>
