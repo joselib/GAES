0129bb304d5ac67e75b6709f1f03b31dccdbd7bd
joselib <joselrn01@gmail.com>
2024-10-12 18:39:21 -0500

    add views users

--- app/models/viewsModel.php
+++ app/models/viewsModel.php
@@ -3,7 +3,7 @@
 
     class viewsModel{
         protected function getViewsModels($view){
-            $whiteList=["dashboard", "login"];
+            $whiteList=["dashboard","userNew","userList","userUpdate","userSearch","userPhoto","logOut","login"];
 
             if(in_array($view,$whiteList)){
                 if(is_file("./app/views/content/".$view."-view.php")){
--- app/views/content/dashboard-view.php
+++ app/views/content/dashboard-view.php
@@ -1,46 +1,26 @@
-<div class="container-fluid p-0">
-  <div class="row g-0 full-height">
-    <!-- Sidebar -->
-    <div class="col-auto px-0 bg-light sidebar" style="width: 250px;"> <!-- Adjust width as needed -->
-      <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-        <a href="<?php echo APP_URL; ?>dashboard/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <img src="<?php echo APP_URL; ?>app/views/img/gaes_logo.png" alt="Logo" width="200" height="100" class="d-inline-block align-top">
-        </a>
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-          <li class="nav-item">
-            <a href="<?php echo APP_URL; ?>dashboard/" class="nav-link align-middle px-0">
-              <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-              <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Usuarios</span>
-            </a>
-            <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-              <li class="w-100">
-                <a href="<?php echo APP_URL; ?>userNew/" class="nav-link px-0"> <span class="d-none d-sm-inline">Nuevo</span></a>
-              </li>
-              <li>
-                <a href="<?php echo APP_URL; ?>userList/" class="nav-link px-0"> <span class="d-none d-sm-inline">Lista</span></a>
-              </li>
-              <li>
-                <a href="<?php echo APP_URL; ?>userSearch/" class="nav-link px-0"> <span class="d-none d-sm-inline">Buscar</span></a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <hr>
-        <div class="dropdown pb-4">
-          <a href="#" class="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="<?php echo APP_URL; ?>app/views/photos/photo_default_user.svg" alt="hugenerd" width="30" height="30" class="rounded-circle">
-            <span class="d-none d-sm-inline mx-1">** <?php /* echo $_SESSION['usuario']; */ ?> **</span>
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="<?php /* echo APP_URL."userUpdate/".$_SESSION['id']."/";  */ ?>">Mi cuenta</a></li>
-            <li><a class="dropdown-item" href="<?php /* echo APP_URL."userPhoto/".$_SESSION['id']."/"; */ ?>">Mi foto</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="<?php echo APP_URL."logOut/"; ?>" id="btn_exit">Salir</a></li>
-          </ul>
+<!-- Main content area -->
+    <div class="col main-content">
+      <div class="container-fluid h-100 d-flex flex-column justify-content-start align-items-center py-3">
+        <h1 class="display-4 text-center mb-4">Home</h1>
+        <div class="row justify-content-center">
+          <div class="col-auto">
+            <figure class="figure">
+              <?php /*
+              if(is_file("./app/views/fotos/".$_SESSION['foto'])){
+                echo '<img src="'.APP_URL.'app/views/fotos/'.$_SESSION['foto'].'" class="figure-img img-fluid rounded-circle" style="width: 128px; height: 128px; object-fit: cover;" alt="User profile picture">';
+              } else {
+                echo '<img src="'.APP_URL.'app/views/fotos/default.png" class="figure-img img-fluid rounded-circle" style="width: 128px; height: 128px; object-fit: cover;" alt="Default profile picture">';
+              }
+              */ ?>
+            </figure>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-auto">
+            <h2 class="h3 text-center">¡Bienvenido <?php /* echo htmlspecialchars($_SESSION['nombre']." ".$_SESSION['apellido'], ENT_QUOTES, 'UTF-8');*/?>!</h2>
+          </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
