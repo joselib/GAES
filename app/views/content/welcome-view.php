ca05dd97de67c15739b45ad36f8e045a70e3a9de
joselib <joselrn01@gmail.com>
2024-10-14 20:50:42 -0500

    template welcome

--- app/views/content/welcome-view.php
+++ app/views/content/welcome-view.php
@@ -1,1 +1,33 @@
-welcome
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Tu Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios Disponibles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reclutamiento y Selección</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cursos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contáctenos</a>
+                    </li>
+                </ul>
+                <a href="<?php echo APP_URL; ?>login/" class="btn btn-primary">Iniciar Sesión</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <h1>Bienvenido a nuestra aplicación</h1>
+        <p>Contenido de bienvenida aquí...</p>
+    </div>
