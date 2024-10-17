4c9cdd0d16308f8a4b3177943d88b16fa9472aec
joselib <joselrn01@gmail.com>
2024-10-16 21:28:42 -0500

    FIXED styles css in navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,47 +1,57 @@
-
-<div class="container-fluid p-0">
-  <div class="row g-0 full-height">
+<div class="container-fluid">
+  <div class="row">
     <!-- Sidebar -->
-    <div class="col-auto px-0 bg-light sidebar" style="width: 250px;"> <!-- Adjust width as needed -->
-      <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-        <a href="<?php echo APP_URL; ?>dashboard/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <img src="<?php echo APP_URL; ?>app/views/img/gaes_logo.png" alt="Logo" width="200" height="100" class="d-inline-block align-top">
-        </a>
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-          <li class="nav-item">
-            <a href="<?php echo APP_URL; ?>dashboard/" class="nav-link align-middle px-0">
-              <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-              <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Usuarios</span>
-            </a>
-            <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-              <li class="w-100">
-                <a href="<?php echo APP_URL; ?>userNew/" class="nav-link px-0"> <span class="d-none d-sm-inline">Nuevo</span></a>
-              </li>
-              <li>
-                <a href="<?php echo APP_URL; ?>userList/" class="nav-link px-0"> <span class="d-none d-sm-inline">Lista</span></a>
-              </li>
-              <li>
-                <a href="<?php echo APP_URL; ?>userSearch/" class="nav-link px-0"> <span class="d-none d-sm-inline">Buscar</span></a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <hr>
+    <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky d-flex flex-column justify-content-between pt-3 h-100">
+        <div>
+          <a href="<?php echo APP_URL; ?>dashboard/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <img src="<?php echo APP_URL; ?>app/views/img/gaes_logo.png" alt="Logo" width="160" height="80" class="d-inline-block align-top">
+          </a>
+          <hr>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?php echo APP_URL; ?>dashboard/">
+                <i class="bi bi-speedometer2"></i>
+                Dashboard
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#userSubmenu" data-bs-toggle="collapse" aria-expanded="false">
+                <i class="bi bi-people"></i>
+                Usuarios
+              </a>
+              <ul class="collapse nav flex-column ms-1" id="userSubmenu">
+                <li class="nav-item">
+                  <a class="nav-link" href="<?php echo APP_URL; ?>userNew/">Nuevo</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="<?php echo APP_URL; ?>userList/">Lista</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="<?php echo APP_URL; ?>userSearch/">Buscar</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
         <div class="dropdown pb-4">
           <a href="#" class="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="<?php echo APP_URL; ?>app/views/photos/photo_default_user.svg" alt="hugenerd" width="30" height="30" class="rounded-circle">
-            <span class="d-none d-sm-inline mx-1">** <?php  echo $_SESSION['user'];  ?> **</span>
+            <img src="<?php echo APP_URL; ?>app/views/photos/photo_default_user.svg" alt="User Photo" width="32" height="32" class="rounded-circle me-2">
+            <strong><?php echo $_SESSION['user']; ?></strong>
           </a>
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="<?php  echo APP_URL."userUpdate/".$_SESSION['id']."/";  ?>">Mi cuenta</a></li>
-            <li><a class="dropdown-item" href="<?php  echo APP_URL."userPhoto/".$_SESSION['id']."/"; ?>">Mi foto</a></li>
+            <li><a class="dropdown-item" href="<?php echo APP_URL."userUpdate/".$_SESSION['id']."/"; ?>">Mi cuenta</a></li>
+            <li><a class="dropdown-item" href="<?php echo APP_URL."userPhoto/".$_SESSION['id']."/"; ?>">Mi foto</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="<?php echo APP_URL."logOut/"; ?>" id="btn_exit">Salir</a></li>
           </ul>
         </div>
       </div>
-    </div>
+    </nav>
+
+    <!-- Main content -->
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <!-- Your main content goes here -->
+    </main>
+  </div>
+</div>
