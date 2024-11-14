71cb56420122d6867044ca50dd656e4424ec8b6d
joselib <joselrn01@gmail.com>
2024-11-14 15:44:40 -0500

    template welcomen frond-end

--- app/views/content/welcome-view.php
+++ app/views/content/welcome-view.php
@@ -1,1 +1,165 @@
-welcome
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="<?php echo APP_URL; ?>app/views/img/gaes_logo.png" alt="Logo" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reclutamiento y Selección</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cursos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contáctenos</a>
+                    </li>
+                </ul>
+                <a href="<?php echo APP_URL; ?>login/" class="btn btn-primary">Iniciar Sesión</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Hero Area Start -->
+    <section id="hero-area" class="bg-light py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7">
+                    <h1 class="display-4 mb-4">Bienvenidos a HR GAES SOFTWARE</h1>
+                    <p class="lead mb-4">Una plataforma Moderna y Simple para la gestion de HH RR para tu negocio.</p>
+                    <div class="d-flex">
+                        <a href="<?php echo APP_URL; ?>login/" class="btn btn-primary me-3">Iniciar Sesión</a>
+                        <a href="#" class="btn btn-outline-primary">Leer mas..</a>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <img src="<?php echo APP_URL; ?>app/views/img/into_welcome.svg" alt="Logo" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Area End -->
+
+    <!-- Services Section Start -->
+    <section id="services" class="py-5">
+        <div class="container">
+            <h2 class="text-center mb-5">Nuestros servicios</h2>
+            <div class="row g-4">
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <i class="bi bi-gear fs-1 text-primary mb-3"></i>
+                            <h5 class="card-title">Capacitaciones y desarrollo</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <i class="bi bi-graph-up fs-1 text-primary mb-3"></i>
+                            <h5 class="card-title">Bienestar y beneficios</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <i class="bi bi-people fs-1 text-primary mb-3"></i>
+                            <h5 class="card-title"> Diversidad e inclusión</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row g-4">
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <i class="bi bi-gear fs-1 text-primary mb-3"></i>
+                            <h5 class="card-title">Gestión del Talento</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <i class="bi bi-graph-up fs-1 text-primary mb-3"></i>
+                            <h5 class="card-title">Gestión Documental</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <i class="bi bi-people fs-1 text-primary mb-3"></i>
+                            <h5 class="card-title"> Gestión de nómina</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Services Section End -->
+
+    <!-- About Section Start -->
+    <section id="about" class="py-5 bg-light">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h2 class="mb-4">Sobre Nosotros</h2>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+                <div class="col-lg-6">
+                    <img src="assets/img/about-image.png" alt="About Image" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- About Section End -->
+
+    <!-- Footer Start -->
+    <footer class="bg-dark text-light py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>HR GAES SOFTWARE</h5>
+                    <p>Una plataforma Moderna y Simple para la gestion de personal para tu negocio.</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#" class="text-light">Inicio</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-light">Servicios</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-light">Reclutamiento y Selección</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-light">Cursos</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-light">Contáctenos</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>&copy; 2024  RH GAES SOFWARE. All rights reserved. by Apredices Servicio Nacional de Aprendizaje SENA</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer End -->
